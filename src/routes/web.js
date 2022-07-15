ec9423d5abf1f9f46e48ae6607477a73fbf3ddd5
quangtienftu49 <50720010+quangtienftu49@users.noreply.github.com>
2022-07-14 21:40:48 -0400

    deploy again

--- src/routes/web.js
+++ src/routes/web.js
@@ -6,8 +6,8 @@ let router = express.Router();
 let initWebRoutes = (app) => {
   router.get("/", homeController.getHomePage);
 
-  router.post("/webbook", homeController.postWebhook);
-  router.get("/webbook", homeController.getWebhook);
+  router.post("/webhook", homeController.postWebhook);
+  router.get("/webhook", homeController.getWebhook);
 
   return app.use("/", router);
 };
