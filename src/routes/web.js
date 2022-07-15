b8e7d9d227ce426d13c10662f031eec87e233ae6
quangtienftu49 <50720010+quangtienftu49@users.noreply.github.com>
2022-07-14 20:38:47 -0400

    add Webhook get and post api

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,11 +1,13 @@
 import express from "express";
+import homeController from "../controllers/HomeController";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-  router.get("/", (req, res) => {
-    return res.send("Hello World");
-  });
+  router.get("/", homeController.getHomePage);
+
+  router.post("/webbook", homeController.postWebhook);
+  router.get("/webbook", homeController.getWebhook);
 
   return app.use("/", router);
 };
